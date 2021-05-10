527aa233b62bb473aadd8152f4835e6730779c92
Romina Schann <cuqi213@gmail.com>
2021-05-11 01:20:00 +0900

    useEffect last

--- src/Detail.js
+++ src/Detail.js
@@ -2,20 +2,28 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
-  let [alert, mAlert] = useState(1);
+  let [alert, mAlert] = useState(true);
+  let [input, mInput] = useState("");
   useEffect(() => {
     setTimeout(() => {
-      mAlert(0);
+      mAlert(false);
     }, 3000); //opacity 말고 modal 처럼 삼항연산자 써서 하는 방식으로 알려주심
     //opacity는 창이 존재하기때문에 화면상 위치를 계속 차지하는 단점이 있음.
-  })
+  }, []);
   let { id } = useParams();
   return (
     <div className="item">
+      {
+        alert === true 
+        ? <div className="alert">
+            <p>It just left 3 stock! Order quickly!</p>
+          </div>
+        : null
+      }
 
-      <div className="alert" style={{opacity:alert}}>
-        <p>It just left 3 stock! Order quickly!</p>
-      </div>
+      <input type="text" placeholder="Write something" 
+      onChange={ (e) => { mInput(e.target.value) }} />
+      <p>{input}</p>
 
       <div className="item__img" >
         <img src={props.items[id].img} alt={props.items[id].title} />
